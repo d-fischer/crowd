14f11949f2ed7a9cce967753efbf2173934389c1
Daniel Fischer <daniel@d-fischer.dev>
2025-11-25 10:22:05 +0900

    use npm instead of yarn

--- src/utils/lifecycle.ts
+++ src/utils/lifecycle.ts
@@ -2,5 +2,5 @@ import type { PackageJson } from 'type-fest';
 import { execProcess } from './process.js';
 
 export async function runLifecycle(cwd: string, scriptName: keyof PackageJson.Scripts, args: string[] = []) {
-	await execProcess('yarn', ['run', '--ignore-scripts', scriptName, ...args], cwd);
+	await execProcess('npm', ['run', '--ignore-scripts', scriptName, ...args], cwd);
 }
